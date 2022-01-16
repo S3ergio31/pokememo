0b2547e447ba985b66c77f367431d7cd94ac9a6c
Sergio Fidelis <sergiofidelis31@hotmail.com>
2022-01-16 13:38:27 -0300

    GameProvider has been moved from index.js to App

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import Tablero from 'components/Tablero';
 import Header from 'components/Header';
 import Body from 'components/Body';
 import MejoresPuntajes from 'components/MejoresPuntajes';
+import GameProvider from 'context/GameProvider';
 
 const App = () => (
-    <>
+    <GameProvider>
         <MejoresPuntajes />
         <Header />
         <Body>
             <Tablero />
         </Body>
-    </>
+    </GameProvider>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'App';
-import GameProvider from 'context/GameProvider';
 
 ReactDOM.render(
     <React.StrictMode>
-        <GameProvider>
-            <App />
-        </GameProvider>
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 );
